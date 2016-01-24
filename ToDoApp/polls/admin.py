8d564dca606d2650edc88fdf95ae838c21a62b2e
Andrea McIntosh <akmcinto@ualberta.ca>
2016-01-24 13:22:20 -0700

    Doing poll tutorial

--- ToDoApp/polls/admin.py
+++ ToDoApp/polls/admin.py
@@ -1,4 +1,33 @@
-from django.contrib import admin
-from .models import Question
+"""
+   Copyright 2016 Andrea McIntosh
 
-admin.site.register(Question)
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ """
+
+from django.contrib import admin
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                ('Date information', {'fields': ['pub_date']}),
+                ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
